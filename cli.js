dd8bd907f39ccb0547d331ff0845e00f594ba527
miracletjf <1398988546@qq.com>
2020-08-18 00:19:04 +0800

    feat: 单个任务操作 完善。 标记已完成，标记未完成，更新标题，删除任务

--- cli.js
+++ cli.js
@@ -49,12 +49,12 @@ function askQuestionForListOperate (todoList) {
     .prompt([
       {
         type: 'list',
-        name: 'key',
+        name: 'index',
         message: '你想要执行的操作是？',
         choices: [
           {
             name: '退出',
-            value: -1
+            value: -2
           },
           ...todoList.map((item, index) => ({
             name: `[${item.isFinish ? 'X':'_'}] ${index + 1} ${item.title}`,
@@ -62,19 +62,20 @@ function askQuestionForListOperate (todoList) {
           })),
           {
             name: '+ 创建任务',
-            value: -2
+            value: -1
           }
         ],
       }
     ])
     .then((answers) => {
-      const key = answers.key
-      if (key === -1) {
-        return false
+      const index = answers.index
+      if (index > -1) {
+        ackQuestionFormItemOperate(todoList, index)
       }
-      if (key === -2) {
+      if( index === -1) {
         executeCreate(todoList)
       }
+
     });
 }
 
@@ -84,17 +85,69 @@ function executeCreate (todoList) {
       {
         type: 'input',
         name: 'title',
-        message: 'How many do you need?'
+        message: '请输入任务名称'
       }
     ])
     .then((answers) => {
-      if (answers.title.toString().trim()) {
+      const title = answers.title.toString().trim()
+      if (title) {
         todoList.push({title: answers.title, isFinish: false})
         db.write(JSON.stringify(todoList))
       }
     });
 }
 
+function ackQuestionFormItemOperate (todoList, index) {
+  const actions = {markAsDone, markAsUndone, updateTitle, remove}
+  inquirer
+    .prompt([
+      {
+        type: 'list',
+        name: 'action',
+        message: '请选择执行的操作',
+        choices: [
+          { name: '退出', value: 'quite' },
+          { name: '已完成', value: 'markAsDone' },
+          { name: '未完成', value: 'markAsUndone' },
+          { name: '改标题', value: 'updateTitle' },
+          { name: '删除', value: 'remove' }
+        ]
+      }
+    ])
+    .then((answers) => {
+      const action = actions[answers.action]
+      action && action(todoList, index)
+    });
+}
 
+function markAsDone(todoList, index) {
+  todoList[index].isFinish = true
+  db.write(JSON.stringify(todoList))
+}
+function markAsUndone(todoList, index) {
+  todoList[index].isFinish = false
+  db.write(JSON.stringify(todoList))
+}
+function updateTitle(todoList, index) {
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'title',
+        message: '请输入新任务名'
+      }
+    ])
+    .then((answers) => {
+      const title = answers.title.toString().trim()
+      if (title) {
+        todoList[index].title = title
+        db.write(JSON.stringify(todoList))
+      }
+    });
+}
+function remove(todoList, index) {
+  todoList.splice(index, 1)
+  db.write(JSON.stringify(todoList))
+}
 
 program.parse(process.argv);
