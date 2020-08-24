c8d244a4cd333cb218e4cedcd67f7ecd95f86977
miracletjf <1398988546@qq.com>
2020-08-25 00:37:49 +0800

    test: 添加测试文件

--- db.js
+++ db.js
@@ -3,21 +3,21 @@ const path = require('path')
 const homeDir = process.env.HOME || require('os').homedir()
 const dbPath = path.join(homeDir, '.todo')
 
-function read () {
+function read (path = dbPath) {
   return new Promise((resolve, reject) => {
-    fs.readFile(dbPath, { flag: 'a+' }, (err, data) => {
+    fs.readFile(path, { flag: 'a+' }, (err, data) => {
       if (err) return reject(err)
       return resolve(JSON.parse(data.toString() || '[]'))
     });
   })
 
 }
-function write (list) {
+function write (list, path = dbPath) {
   return new Promise((resolve, reject) => {
     if (!list instanceof Array) {
       return reject(new Error('数据写入类型有误，请检查后再试！'))
     }
-    fs.writeFile(dbPath, JSON.stringify(list), err => {
+    fs.writeFile(path, JSON.stringify(list), err => {
       if (err) return reject(err);
       return resolve()
     })
