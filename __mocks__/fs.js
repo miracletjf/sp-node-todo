9d62f57446f44b174174bc4161ed57e70966def1
miracletjf <1398988546@qq.com>
2020-08-25 23:40:56 +0800

    test: 添加测试文件写入 测试用例

--- __mocks__/fs.js
+++ __mocks__/fs.js
@@ -18,6 +18,20 @@ fs.readFile = (path, options, callback) => {
   }
 }
 
+let writeMockData = {}
+
+fs.setWriteFileMock = (path, fn) => {
+  return writeMockData[path] = fn
+}
+
+fs.writeFile = (path, data, callback) => {
+  if (path in writeMockData) {
+    writeMockData[path](path, data, callback)
+  } else {
+    _fs.writeFile(path, data, callback)
+  }
+}
+
 fs.clearData = () => {
   readMockData = {}
 }
--- __tests__/db.spec.js
+++ __tests__/db.spec.js
@@ -12,4 +12,14 @@ describe('db', () => {
     const list = await db.read('/xxx')
     expect(list).toEqual(data)
   })
+  it('can be write', async () => {
+    let fakeData
+    fs.setWriteFileMock('/yyy',(path, data, callback) => {
+      fakeData = data
+      callback(null)
+    })
+    const list = [{name: 'ggg', age: 16}, {name: 'sss', age: 24}]
+    await db.write(list, '/yyy')
+    expect(fakeData).toBe(JSON.stringify(list))
+  })
 })
